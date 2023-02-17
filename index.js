ae9a4a83c6b992032d33a7c8f149b8deab495456
Daedalus2K <apolloswatered@gmail.com>
2023-02-17 00:08:50 -0500

    commit tree 6320e38c602b85ad5b3a4644ed859faaafaa2d17

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+
+    function clickAlert() {
+         alert('I was clicked!'); //Two arguments:the name of the EVENT to listen for, and
+                                    //a CALLBACK function to "handle" the event
+        }
+        input.addEventListener('click', clickAlert);
+        console.log(addingEventListener);
 }
+
